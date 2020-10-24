a8951d843987ec8090e8c2cd9258963a3734af67
achuttarsing <adriencdelafontaine@yahoo.fr>
2020-10-24 17:49:11 +0200

    correct setup.py indentation

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
-  # setup.py
-  from setuptools import setup
+# setup.py
+from setuptools import setup
 
-  setup(
-      name="Jupyslack",
-      version="0.1",
-      packages=["jupyslack"],
-      license="MIT",
-      author="Adrien Chuttarsing",
-      author_email="adrien.chuttarsing@gmail.com",
-      url="https://github.com/Achuttarsing/jupyslack",
-      description="Slack integration to notebooks",
-      long_description=open("README.md").read(),
-      keywords="ipython slack notebook jupyslack",
-      install_requires = ['ipython','requests'],
-      classifiers=[
-          "Development Status :: 3 - Alpha",
-          "Intended Audience :: Developers",
-          "Framework :: IPython",
-          "Programming Language :: Python",
-          "Topic :: Utilities",
-      ],
-  )
+setup(
+  name="Jupyslack",
+  version="0.1",
+  packages=["jupyslack"],
+  license="MIT",
+  author="Adrien Chuttarsing",
+  author_email="adrien.chuttarsing@gmail.com",
+  url="https://github.com/Achuttarsing/jupyslack",
+  description="Slack integration to notebooks",
+  long_description=open("README.md").read(),
+  keywords="ipython slack notebook jupyslack",
+  install_requires = ['ipython','requests'],
+  classifiers=[
+      "Development Status :: 3 - Alpha",
+      "Intended Audience :: Developers",
+      "Framework :: IPython",
+      "Programming Language :: Python",
+      "Topic :: Utilities",
+  ],
+)
